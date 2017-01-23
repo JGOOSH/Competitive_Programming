55d4daab99319cdaf520c828a7a0bbb839d61d6b
Jamin Goo <jgoo@utexas.edu>
2017-01-22 20:24:55 -0600

    first problem

---
+++ Java/pb1.java
@@ -0,0 +1,16 @@
+
+public class pb1 {
+
+	public static void main(String[] args) {
+		int sum = 0; 
+		int i = 1; 
+		while(i < 1000){
+			if(i % 3 == 0 || i % 5 == 0){
+				sum+=i;
+			}
+			i++;
+		}
+		System.out.println(sum);
+	}
+
+}
